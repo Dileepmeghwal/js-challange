67278bdc071bd190605a7d7238cce71a8d6eb643
Dileep <dileepmeghwal143@gmail.com>
2025-03-17 12:11:53 +0530

    init

--- Array/searching/01-app.js
+++ Array/searching/01-app.js
@@ -1,9 +1,23 @@
-# js-challange
+// Linear Search Algorithm
 
-# 1. Linear Search Algorithm
+// Input: arr[] = [1, 2, 3, 4], x = 3
+// Output: 2
 
+function search(arr, target) {
+  let index = 0;
+  for (const i of arr) {
+    if (i === target) {
+      return index;
+    }
+    index++;
+  }
+  return -1;
+}
 let arr = [1, 2, 3, 4];
 let arr1 = [34, 3, 25, 84, 28, 97, 63];
+
+console.log(search(arr, 4));
+console.log(search(arr1, 97));
 
 //  using for loop
 
